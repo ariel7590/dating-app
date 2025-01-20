43ef972d14df7caad531028d1c437f9e1527f66f
Ariel Turchinsky <ariel7590@gmail.com>
2025-01-20 23:19:30 +0200

    created an entity, connected the project to sqlite using entity framework

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8078d0d167e4bcd280fbd2bef4fcf59778fe6f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5cb5be39987fd5323883ed1b6fc4ddf6960ee5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
